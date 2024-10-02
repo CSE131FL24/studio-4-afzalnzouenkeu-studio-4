a2451ccf1108ea81fd415d4a52441fadbc9ee5c8
shaafafzal <shaafafzal@localhost>
2024-10-02 14:15:15 -0500

    This is our Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -4,8 +4,26 @@ import java.awt.Color;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-public class Flag {
-	public static void main(String[] args) {
+public class Flag 
+{
+	public static void main(String[] args) 
+	{
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5,0.5,0.1,0.3);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(.3, 0.5, 0.1, 0.3);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(.7, 0.5, 0.1, 0.3);
+		
+		double[] x = {0.5, 0.525, 0.475};
+	    double[] y = {0.525,0.475,0.475};
+	    StdDraw.filledPolygon(x, y);
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,12 +1,13 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+//
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
-
+//import javax.swing.SwingUtilities;
+//
 import edu.princeton.cs.introcs.StdDraw;
 
 /**
@@ -19,6 +20,62 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean status = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double xdist = in.nextDouble();
+		double ydist = in.nextDouble();
+		
+	    Color color = new Color(r, g, b);
+		StdDraw.setPenColor(color);
+		
+		if (shape.equals("rectangle"))
+		{
+			if (status == true)
+			{
+				StdDraw.filledRectangle(x, y, xdist, ydist);
+			}
+			else
+			{
+				StdDraw.rectangle(x, y, xdist, ydist);
+			}
+		}
+			
+		if (shape.equals("ellipse"))
+			if (status == true)
+			{
+				StdDraw.filledEllipse(x, y, xdist, ydist);
+			}
+			else
+			{
+				StdDraw.ellipse(x, y, xdist, ydist);
+			}
+		
+		if (shape.equals("triangle"))
+		{
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			
+			double [] xarray = {x,xdist, p1};
+			double [] yarray = {y,ydist, p2};
+			if (status == true)
+			{
+				StdDraw.filledPolygon(xarray, yarray);
+			}
+			else
+			{
+				StdDraw.polygon(xarray,yarray);
+			}
+		}
+		
+		
+		
+		
+
 		
 	}
 }
